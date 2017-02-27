44775c3402b12b6686424b8638b16926aeaf190c
LiuHaixin <liuhaixin2016@outlook.com>
2017-02-27 18:19:22 +0800

    temp

---
+++ Modules/Modules.AQE/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using Modules.FastReflection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modules.AQE
+{
+    public class Calculator
+    {
+        public static Dictionary<string, IPropertyAccessor> IAQMDataPropertiesDic
+    }
+}
