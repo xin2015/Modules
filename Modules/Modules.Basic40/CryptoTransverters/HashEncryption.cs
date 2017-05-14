fa9b0d829dc77d61ebfdc7688cc6492c7777ba5f
LiuHaixin <liuhaixin2016@outlook.com>
2017-05-14 21:00:27 +0800

    Basic40

---
+++ Modules/Modules.Basic40/CryptoTransverters/HashEncryption.cs
@@ -0,0 +1,35 @@
+﻿using Modules.Basic.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modules.Basic.CryptoTransverters
+{
+    public class HashEncryption : IEncrypt
+    {
+        public static HashEncryption Default { get; set; }
+        static HashEncryption()
+        {
+            Default = new HashEncryption();
+        }
+
+        protected HashAlgorithm HA { get; set; }
+        public HashEncryption()
+        {
+            HA = SHA256.Create();
+        }
+
+        public byte[] Encrypt(byte[] inputBuffer)
+        {
+            return HA.ComputeHash(inputBuffer);
+        }
+
+        public string Encrypt(string inString)
+        {
+            return Encrypt(inString.FromUTF8String()).ToBase64String();
+        }
+    }
+}
